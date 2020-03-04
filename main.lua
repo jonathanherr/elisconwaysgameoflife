aceb32c4118bcc5685d3f91173982970e30567af
jonathan <jonathan@blackboiler.com>
2020-03-04 18:15:32 -0500

    * now detecting when a cell has a left neighbor!

--- main.lua
+++ main.lua
@@ -8,9 +8,13 @@ function love.load()
 	cellsize=25
 	cellnum = 0
 	draggingEnabled = false
+	LEFT=0
+	RIGHT=1
+	ABOVE=2
+	BELOW=3
 	lastCell = {["h"]=-1,["w"]=-1}
 	love.window.setTitle("Eli's Conway's Game Of Life")
-	love.window.setMode(width, height, {resizable=true, vsync=false, minwidth=800, minheight=600})
+	love.window.setMode(width+(width/cellsize), height+(height/cellsize), {resizable=true, vsync=true, minwidth=800, minheight=600})
 
 	for h=0,height,cellsize+1 do
 		cells[h]={}
@@ -44,8 +48,6 @@ end
 function love.mousemoved(x,y,dx,dy,istouch)
 	if draggingEnabled then
 		h,w=getCell(x,y)
-		print(h,w)
-		print(inspect(lastCell))
 		if lastCell.h~=h or lastCell.w~=w then
 			h,w=setCell(x,y)
 			lastCell.h=h
@@ -81,6 +83,43 @@ function setCell(x,y)
 	return h,w
 end
 
+function alive(h,w)
+	if cells[h][w]==0 then
+		return false
+	elseif cells[h][w]==1 then
+		return true
+	end
+end
+function getNeighbor(direction,h,w)
+	totalcellsize=cellsize+1
+	if direction==LEFT then
+		if w-totalcellsize>0 then
+			leftcell_w=w-totalcellsize
+			leftalive=alive(h,leftcell_w)
+			if leftalive then
+				print("left")
+				print(h,leftcell_w)
+			end
+			return leftalive
+		end
+	elseif direction==RIGHT then
+		print("right")
+	elseif direction==ABOVE then
+		print("above")
+	elseif direction==BELOW then
+		print("below")
+	end
+end
 function tick()
-	
+	for h=0,height,cellsize+1 do
+		for w=0,width,cellsize+1 do
+			if alive(h,w) then
+				leftAlive=getNeighbor(LEFT,h,w)
+			end
+			--rightAlive=getNeighbor(RIGHT,h,w)
+			--aboveAlive=getNeighbor(ABOVE,h,w)
+			--belowAlive=getNeighbor(BELOW,h,w)
+
+		end
+	end
 end
